26b8fa88f62ecbf12e38fef1ce5976e2cec7b461
markirius <markbombomb@gmail.com>
2021-07-05 09:43:37 -0300

    added python decouple

--- todolist/settings.py
+++ todolist/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t3ln43a$h2nm&i%_j#5gu4pm8mhkfr#fc_+zrr9dau#fg!4#21'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
